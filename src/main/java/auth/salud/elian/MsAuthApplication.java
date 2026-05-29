a1015c306ce3a65ff71e0f7b9a80aed57a59ff38
Elian Salomon <floreschavezeliansalomon@gmail.com>
2026-05-29 01:08:28 -0600

    feat: initial commit with ms-auth structure

---
+++ src/main/java/auth/salud/elian/MsAuthApplication.java
@@ -0,0 +1,13 @@
+package auth.salud.elian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auth/salud/elian/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package auth.salud.elian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
